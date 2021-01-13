62397386bf435d5c3846583c497e01a052c7eae2
Luca Cattide <info@lucacattide.dev>
2021-01-13 10:55:34 +0100

    fix: Not Found dependency

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import loadable from '@loadable/component';
 
-const EmptyState = loadable(() => import('../EmptyState'));
+const EmptyState = loadable(() => import('./EmptyState'));
 const useStyles = makeStyles(() => ({
   emptyIcon: {
     color: '#0000008a',
